839470e5c97ae72a3128b2d5e43bc1d4cebb6aee
LouisAskia <louis@askia.com>
2016-08-01 12:32:29 +0200

    first commit

--- app/publisher/ADXPublisherGitHub.js
+++ app/publisher/ADXPublisherGitHub.js
@@ -6,17 +6,13 @@ var fs              = require('fs');
 var path            = require('path');
 var async           = require('async');
 var _               = require('underscore');
-console.log(path.join(__dirname,'../../../ADCs/adc2_gender/'));
 var git             = require('simple-git')();
 
 
-var default_options = {
-    username: 'LouisAskia', //
-    token: '3b94fab02e1cf926056ff07ce29ca4872287a8fd' //Must be a token with delete_repo permisssion
-};
+
 
 git.add('./*'/*path.join(__dirname,'../../../ADCs/adc2_gender/')*/)
     .commit('first commit')
-    .push('origin', 'test');
+    .push('test', 'master');
 
 
